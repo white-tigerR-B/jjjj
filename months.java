bf6b0af554a838e4fe50f88a927c87538e2ccba0
white tiger B <bahugunarambo@gmail.com>
2021-08-27 19:34:12 +0530

    months

---
+++ months.java
@@ -0,0 +1,66 @@
+
+public class months{
+            
+           public static void main(String Args[])
+             {
+               
+              System.out.println("hello");  
+              for(int i=1;i<=12;i++)
+               { 
+                 switch (i) {
+                    case 1:
+                    System.out.println(""+i+ "to jan");
+                    break;
+                    case 2:
+                    System.out.println(""+i+ "to Feb");
+                    break;
+                    case 3  :
+                    System.out.println(""+i+ "to March");
+                    break;
+                    case 4:
+                    System.out.println(""+i+ "to April");
+                    break;
+                    case 5:
+                    System.out.println(""+i+ "Mays");
+                    break;
+                  
+                    case 6:
+                    System.out.println(""+i+ "to june");
+                    break;
+                    case 7:
+                    System.out.println(""+i+ "to july");
+                    break;
+                    case 8:
+                    System.out.println(""+i+ "to August");
+                    break;
+                    case 9:
+                    System.out.println(""+i+ "to September");
+                    break;
+                    case 10:
+                    System.out.println(""+i+ "to October");
+                    break;
+                    case 11:
+                    System.out.println(""+i+ "to November");
+                    break;
+                    case 12:
+                    System.out.println(""+i+ " to December");
+                    break;
+                
+                  
+                    default:
+                    break;
+                                   
+                  }
+               }
+                   
+                 
+                 
+
+                                 
+                                 
+                }
+             
+
+
+            
+}
