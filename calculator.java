4415953a7019b174d8577524923a49eb8db833a5
white tiger B <bahugunarambo@gmail.com>
2021-08-27 17:28:44 +0530

    kam.

--- calculator.java
+++ calculator.java
@@ -2,8 +2,7 @@ class calculator
 {
    public static void main(String []args)
 {
-     SYSY
-
+     rrrr
      int a=Integer.parseInt(args[0]);
      int b=Integer.parseInt(args[1]);
      System.out.println("hey I am a calculator");
